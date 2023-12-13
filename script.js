a3fe38d7490a6c0fd4ff202413f88eae8d0ef5b4
ronok420 <shahariarronok41.com>
2023-12-13 10:41:11 +0600

    added displaWeather function to show all information from the fetched api

--- script.js
+++ script.js
@@ -6,10 +6,41 @@ function searchWeather() {
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
+            displayWeather(data);
         })
         .catch(error => {
             console.error('Error fetching weather data:', error);
             alert('Error fetching weather data. Please try again.');
         });
+}
+function displayWeather(data) {
+    const weatherDataDiv = document.getElementById('weatherData');
+    weatherDataDiv.innerHTML = '';
+
+   
+    const cityName = data.name;
+    const country = data.sys.country;
+    const temperature = Math.round(data.main.temp - 273.15); // Convert to Celsius
+    const weatherDescription = data.weather[0].description;
+    const icon = data.weather[0].icon;
+    const humidity = data.main.humidity;
+    const windSpeed = data.wind.speed;
+    const pressure = data.main.pressure;
+
+
+   
+    
+
+    const weatherInfo = document.createElement('div');
+    weatherInfo.innerHTML = `
+        <h2>${cityName}, ${country}</h2>
+        <p>Temperature: ${temperature}°C</p>
+        <p>Weather: ${weatherDescription}</p>
+        <img src="http://openweathermap.org/img/wn/${icon}.png" alt="Weather icon">
+        <p>Humidity: ${humidity}</p>
+        <p>windSpeed: ${windSpeed}</p>
+        <p>pressure: ${pressure}</p>
+    `;
+
+    weatherDataDiv.appendChild(weatherInfo);
 }
